dc26cbdc5a5a6cf14375337fa266e90037fd6e84
Tom Alvarez <tmlvrz@gmail.com>
2024-09-03 15:54:40 +0200

    working on rps program

--- script.js
+++ script.js
@@ -8,4 +8,31 @@ function getComputerChoice () {
         return 'scissors';
     }
 }
-console.log(getComputerChoice());
+
+
+function getHumanChoice () {
+    const input = prompt('Rock, Paper or Scissors?').toLowerCase();
+    const choices = ['rock', 'paper', 'scissors'];
+
+    if (!choices.includes(input)) {
+        alert('your choice is invalid');
+        return;
+    }
+    const computerChoice = getComputerChoice();
+    alert(`Computer chose: ${computerChoice}`);
+
+    if (input === computerChoice) {
+        alert('draw!');     
+    } else if (
+        (input === 'rock' && computerChoice === 'scissors') ||
+        (input === 'paper' && computerChoice === 'rock') ||
+        (input === 'scissors' && computerChoice === 'paper')
+    ) {
+        alert('You win!');
+    } else {
+        alert('Computer wins!');
+    }
+}
+
+let humanScore = 0
+let computerScore = 0
