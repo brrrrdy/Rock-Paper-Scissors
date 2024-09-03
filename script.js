a1d870cdd309427759e367bf039ed17c6d757d30
Tom Alvarez <tmlvrz@gmail.com>
2024-09-03 15:14:36 +0200

    started work on rps program

---
+++ script.js
@@ -0,0 +1,20 @@
+function getComputerChoice (r,p,s) {
+const r = 1;
+const p = 2;
+const s = 3;
+return Math.random() * (1-3) + min;
+console.log(getComputerChoice);
+}
+
+/*
+
+define function and parameters
+create variables for
+    1 = rock
+    2 = paper
+    3 = scissors
+return random math between 1 and 3
+return Math.random() * (max - min) + min;
+
+
+*/
