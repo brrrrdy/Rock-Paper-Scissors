ca97926f951db18641b3fff3dd865c73bdfa220f
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2024-10-23 16:37:09 +0200

    planning

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ function getComputerChoice() {
 // get the human's choice and determine the winner
 
 function getHumanChoice() {
-    const input = prompt('rock, paper or scissors?').toLowerCase();
+    const input = prompt('rock, paper or scissors?').toLowerCase(); // change
     const choices = ['rock', 'paper', 'scissors'];
 
-    if (!choices.includes(input)) {
-        alert('Your choice is invalid');
-        return null; 
-    }
+    if (!choices.includes(input)) { // remove
+        alert('Your choice is invalid'); // remove
+        return null; // remove
+    } // remove
 
     const computerChoice = getComputerChoice();
     alert(`Computer chose: ${computerChoice}`);
@@ -58,7 +58,7 @@ function playRound() {
 
 // Display the current score after each round
 
-    alert(`Score:\nYou: ${humanScore}\nComputer: ${computerScore}`);
+    alert(`Score:\nYou: ${humanScore}\nComputer: ${computerScore}`); // remove
 }
 
 // Function to play the game for multiple rounds
@@ -87,3 +87,39 @@ function playGame() {
 // Start game
 
 playGame();
+
+
+/*
+
+REQUIREMENT:
+
+- Game can be played by clicking buttons instead of prompt input
+
+SOLUTION:
+
+- Change input to handle button clicks for the three buttons instead of requiring text input.
+- Create buttons on html doc
+- Create event listener on js doc that call the playRound function with the correct humanChoice.
+    event listener should:
+        - determine which of the buttons has been selected.
+        - pass that choice into playRound.
+
+- No need for validity checker. remove.
+
+REQUIREMENT:
+
+- Display the result tallies on the page instead of as an alert.
+
+SOLUTION:
+
+- Create a div in html for displaying result tallies.
+- Display humanscore and computerScore on each.
+    pass the value of humanScore and computerScore into the html div using getElementById (https://stackoverflow.com/questions/9341209/pass-a-value-from-javascript-to-html)
+
+- remove score alert.
+- retain result of current round as an alert.
+- ensure that once the alert has been read and cancelled, focus back on the buttons.
+
+
+
+*/
